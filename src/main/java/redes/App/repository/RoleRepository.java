8d342516b49c0c9cabd81b0c0dfb977ecefb5bf3
pavega <vega.vasquezpao@gmail.com>
2022-10-25 22:21:40 -0600

    Revert "Project created"
    This reverts commit ae4c49432795a73d90113c86ee520169349f96fa.

--- src/main/java/redes/App/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package redes.App.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import redes.App.domain.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
--- src/main/java/redes/App/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package redes.App.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import redes.App.domain.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-    User findByEmail(String email);
-}
