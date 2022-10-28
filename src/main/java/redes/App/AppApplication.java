cf6f2e9e439aa423fc915127332fa3a444dd9238
LuisGa24 <69638645+LuisGa24@users.noreply.github.com>
2022-10-28 01:58:49 -0600

    Update AppApplication.java

--- src/main/java/redes/App/AppApplication.java
+++ src/main/java/redes/App/AppApplication.java
@@ -12,7 +12,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan("redes.App.domain")
 @EnableJpaRepositories("redes.App.repository")
 public class AppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
