9986b803bb31b760c9089c8900b336f4b6ddf88c
LuisGa24 <69638645+LuisGa24@users.noreply.github.com>
2022-10-28 00:37:56 -0600

    Update AppApplicationTests.java

--- src/test/java/redes/App/AppApplicationTests.java
+++ src/test/java/redes/App/AppApplicationTests.java
@@ -3,7 +3,7 @@ package redes.App;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootApplication
 class AppApplicationTests {
 
 	@Test
