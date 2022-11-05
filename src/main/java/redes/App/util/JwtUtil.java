e63aa20c980d43907626fc70344eef0194ab5263
pavega <vega.vasquezpao@gmail.com>
2022-11-04 18:35:00 -0600

    change en jwtutil

--- src/main/java/redes/App/util/JwtUtil.java
+++ src/main/java/redes/App/util/JwtUtil.java
@@ -66,6 +66,7 @@ public class JwtUtil {
         return Jwts.builder()
                 .setSubject(userDetails.getEmail())
                 .claim("role", userDetails.getRole().getName())
+                .claim("name", userDetails.getName())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + TOKEN_VALIDITY * 1000))
                 .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
