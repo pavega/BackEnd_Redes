58f85487026b88ffb2435b8a77db477ff5d3b34c
LuisGa24 <69638645+LuisGa24@users.noreply.github.com>
2022-11-04 18:38:49 -0600

    Sincronización (#3)
    * Update AppApplication.java
    
    * Update JwtAuthenticationEntryPoint.java
    
    * Update JwtAuthenticationEntryPoint.java
    
    * Back end termindao (#1)
    
    Co-authored-by: unknown <allcincoceroseis@gmail.com>
    
    * Test https (#2)
    
    * update https
    
    * Update application.properties
    
    * Update application.properties
    
    * Update application.properties
    
    * update
    
    * Update application.properties
    
    * update
    
    * Update AppApplication.java
    
    * up
    
    * Update application.properties
    
    Co-authored-by: unknown <allcincoceroseis@gmail.com>

--- src/main/java/redes/App/AppApplication.java
+++ src/main/java/redes/App/AppApplication.java
@@ -1,20 +1,61 @@
-package redes.App.demo;
+package redes.App;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"redes.App.*"})
 @ComponentScan({"redes.App.controller", "redes.App.repository", "redes.App.service", "redes.App.domain",
-		"redes.App.configuration", "redes.App.util"})
+        "redes.App.configuration", "redes.App.util"})
 @EntityScan("redes.App.domain")
 @EnableJpaRepositories("redes.App.repository")
 public class AppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
+//    @Bean
+//    public ServletWebServerFactory servletContainer() {
+//        // Enable SSL Trafic
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint securityConstraint = new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                securityConstraint.addCollection(collection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+//
+//        // Add HTTP to HTTPS redirect
+//        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+//
+//        return tomcat;
+//    }
+//
+//    /*
+//    We need to redirect from HTTP to HTTPS. Without SSL, this application used
+//    port 8097. With SSL it will use port 8443. So, any request for 8097 needs to be
+//    redirected to HTTPS on 8443.
+//     */
+//    private Connector httpToHttpsRedirectConnector() {
+//        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+//        connector.setScheme("http");
+//        connector.setPort(8097);
+//        connector.setSecure(false);
+//        connector.setRedirectPort(8443);
+//        return connector;
+//    }
 
 }
--- src/test/java/redes/App/AppApplicationTests.java
+++ src/test/java/redes/App/AppApplicationTests.java
@@ -3,7 +3,7 @@ package redes.App;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = redes.App.demo.AppApplication.class)
+@SpringBootTest(classes = redes.App.AppApplication.class)
 class AppApplicationTests {
 	@Test
 	void contextLoads() {
